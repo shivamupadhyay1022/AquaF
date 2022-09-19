7278b3556f8eadbfb6e8f7d72dae601bbae29e8c
shivam <shivamupadhyay1022>
2022-09-20 00:08:50 +0530

    v0.0.2

--- app/src/main/java/com/su/aquaf/MainActivity.java
+++ app/src/main/java/com/su/aquaf/MainActivity.java
@@ -1,74 +1,109 @@
 package com.su.aquaf;
 
-import android.app.FragmentManager;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
+import androidx.appcompat.view.menu.MenuPopupHelper;
+import androidx.fragment.app.Fragment;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.RelativeLayout;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
 import com.su.aquaf.databinding.ActivityMainBinding;
-import com.su.aquaf.ui.HomeFragment;
-import com.su.aquaf.ui.PartnersFragment;
-import com.su.aquaf.ui.ServicesFragment;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
-    RelativeLayout topb;
-//    HomeFragment homeFragment;
-//    ServicesFragment servicesFragment;
-//    PartnersFragment partnersFragment;
+    ActivityMainBinding binding2;
+    ImageView spinner;
+    MenuBuilder menu_builder;
 
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView tv4 = findViewById(R.id.topbar_tittle);
+        spinner = findViewById(R.id.spinner_menu);
 
-        TextView tv = (TextView) findViewById(R.id.topbar_tittle2);
-        tv.setText("Blah: ");
-          // for refreshment
+        menu_builder = new MenuBuilder(this);
+        MenuInflater inflater = new MenuInflater(this);
+        inflater.inflate(R.menu.menu_setting, menu_builder);
 
-        Fragment homeFragment = new HomeFragment();
-        openFragment(homeFragment);
+        spinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MenuPopupHelper optionMenu = new MenuPopupHelper(MainActivity.this,
+                        menu_builder,view);
+                optionMenu.setForceShowIcon(true);
 
+                menu_builder.setCallback(new MenuBuilder.Callback() {
+                    @Override
+                    public boolean onMenuItemSelected(@NonNull MenuBuilder menu, @NonNull MenuItem item) {
+                        switch (item.getItemId())
+                        {
+                            case R.id.nav_home:
+                                Toast.makeText(MainActivity.this, "Clicked on home", Toast.LENGTH_SHORT).show();
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        binding.navView.setOnItemSelectedListener(item -> {
+                        }
+                        return false;
+                    }
 
-            switch (item.getItemId()){
+                    @Override
+                    public void onMenuModeChange(@NonNull MenuBuilder menu) {
 
-                case R.id.navigation_home:
-                    tv.append("Home");
-                    Fragment home2Fragment = new HomeFragment();
-                    openFragment(home2Fragment);
-                    break;
-                case R.id.navigation_services:
-                    tv.setText("Services");
-                    Fragment serviceFragment = new ServicesFragment();
-                    openFragment(serviceFragment);
-                    break;
-                case R.id.navigation_partners:
-                    tv.setText("Partners");
-                    Fragment partnersFragment = new PartnersFragment();
-                    openFragment(partnersFragment);
-                    break;
+                    }
+                });
+
+                optionMenu.show();
+
             }
-
-            return true;
         });
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        tv4.setText("Home");
+                        return true;
+                    case R.id.navigation_services:
+                        Toast.makeText(MainActivity.this, "Services", Toast.LENGTH_SHORT).show();
+                        tv4.setText("Services");
+                        return true;
+                    case R.id.navigation_partners:
+                        Toast.makeText(MainActivity.this, "partners", Toast.LENGTH_SHORT).show();
+                        tv4.setText("Partners");
+                        return true;
+                }
+                return true;
+            }
+        });
+
 
     }
 
-    public void openFragment(Fragment fragment) {
-//        tittle_text.setText("Home");
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater  = getMenuInflater();
+
+        return true;
+    }
+
+    public void openfragment (Fragment fragment){
         androidx.fragment.app.FragmentManager fm1 = getSupportFragmentManager();
         androidx.fragment.app.FragmentTransaction transaction1 = fm1.beginTransaction();
         transaction1.replace(R.id.fragment_container, fragment);
