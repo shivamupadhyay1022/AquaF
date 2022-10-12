4b6f7ae87f10a09fde69a0b0507a4bbd36250ad7
shivamupadhyay1022 <shivamupadhyay1022@gmail.com>
2022-10-12 18:45:31 +0530

    v0.0.8

--- app/src/main/java/com/su/aquaf/MainActivity.java
+++ app/src/main/java/com/su/aquaf/MainActivity.java
@@ -21,6 +21,11 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.su.aquaf.databinding.ActivityMainBinding;
+import com.su.aquaf.ui.AboutFragment;
+import com.su.aquaf.ui.ContactFragment;
+import com.su.aquaf.ui.HomeFragment;
+import com.su.aquaf.ui.PartnersFragment;
+import com.su.aquaf.ui.ServicesFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = new MenuInflater(this);
         inflater.inflate(R.menu.menu_setting, menu_builder);
 
+        Fragment homefragment = new HomeFragment();
+        openfragment(homefragment);
+
         spinner.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -53,9 +61,18 @@ public class MainActivity extends AppCompatActivity {
                     public boolean onMenuItemSelected(@NonNull MenuBuilder menu, @NonNull MenuItem item) {
                         switch (item.getItemId())
                         {
-                            case R.id.nav_home:
-                                Toast.makeText(MainActivity.this, "Clicked on home", Toast.LENGTH_SHORT).show();
-
+                            case R.id.nav_contact:
+//                                Toast.makeText(MainActivity.this, "Clicked on Contact", Toast.LENGTH_SHORT).show();
+                                Fragment contactfragment = new ContactFragment();
+                                openfragment(contactfragment);
+                                tv4.setText("Contact us");
+                                break;
+                            case R.id.nav_About:
+//                                Toast.makeText(MainActivity.this, "Clicked on Contact", Toast.LENGTH_SHORT).show();
+                                Fragment aboutfragment = new AboutFragment();
+                                openfragment(aboutfragment);
+                                tv4.setText("About Us");
+                                break;
                         }
                         return false;
                     }
@@ -77,15 +94,21 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.navigation_home:
-                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        Fragment homefragment = new HomeFragment();
+                        openfragment(homefragment);
                         tv4.setText("Home");
                         return true;
                     case R.id.navigation_services:
-                        Toast.makeText(MainActivity.this, "Services", Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(MainActivity.this, "Services", Toast.LENGTH_SHORT).show();
+                        Fragment servicesfragment = new ServicesFragment();
+                        openfragment(servicesfragment);
                         tv4.setText("Services");
                         return true;
                     case R.id.navigation_partners:
-                        Toast.makeText(MainActivity.this, "partners", Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(MainActivity.this, "partners", Toast.LENGTH_SHORT).show();
+                        Fragment navigationfragment = new PartnersFragment();
+                        openfragment(navigationfragment);
                         tv4.setText("Partners");
                         return true;
                 }
---
+++ app/src/main/java/com/su/aquaf/ui/ContactFragment.java
@@ -0,0 +1,117 @@
+package com.su.aquaf.ui;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.su.aquaf.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ContactFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ContactFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ContactFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ContactFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ContactFragment newInstance(String param1, String param2) {
+        ContactFragment fragment = new ContactFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_contact, container, false);
+
+        TextView pn1 = view.findViewById(R.id.frag_contact_pn1);
+        SpannableString content = new SpannableString("+919911498738");
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+        pn1.setText(content);
+        pn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(Intent.ACTION_DIAL);
+                intent1.setData(Uri.parse("tel:+919911498738"));
+                startActivity(intent1);
+            }
+        });
+        TextView pn2 = (TextView) view.findViewById(R.id.frag_contact_pn2);
+        SpannableString content2 = new SpannableString("+917982361289");
+        content.setSpan(new UnderlineSpan(), 0, content2.length(), 0);
+        pn1.setText(content2);
+        pn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(Intent.ACTION_DIAL);
+                intent2.setData(Uri.parse("tel:+917982361289"));
+                startActivity(intent2);
+            }
+        });
+
+        return view;
+    }
+
+//    @Override
+//    public void onClick(View view) {
+//        switch (view.getId()) {
+//            case R.id.frag_contact_pn1:
+//                Intent intent = new Intent(Intent.ACTION_DIAL);
+//                intent.setData(Uri.parse("tel:0123456789"));
+//                startActivity(intent);
+//                break;
+//            case R.id.frag_contact_pn2:
+//
+//
+//                break;
+//
+//            default:
+//                break;
+//        }
+//    }
+}
